e7e235c00321a9cd4f608944f2dd273530e7f0b8
Mayank <mayank.g.bth@gmail.com>
2024-04-26 06:43:26 +0530

    Install Django Rest Framework

--- project_configuration/settings.py
+++ project_configuration/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # External libraries
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
