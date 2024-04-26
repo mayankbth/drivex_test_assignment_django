b917c8db54fb4bbd920818ea221f51840152211a
Mayank <mayank.g.bth@gmail.com>
2024-04-27 04:02:07 +0530

    Add BookList api

--- library/rest_apis/urls.py
+++ library/rest_apis/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import (
-    GetBooks
+    GetBooks, BookList
 )
 
 urlpatterns = [
+    # To books in the library
     path('get_books/', GetBooks.as_view(), name='get_books'),
+    
+    path('book_list/', BookList.as_view(), name='book_list'),
 ]
--- library/rest_apis/views.py
+++ library/rest_apis/views.py
@@ -7,6 +7,10 @@ from .helper_functions.url_creators import url_creator
 from .helper_functions.data_extractor import data_extractor_guten_dex
 from .helper_functions.context_generator import context_data_generator
 
+from .serializers import *
+
+from library.models import Book
+
 
 class GetBooks(APIView):
     """
@@ -96,5 +100,41 @@ class GetBooks(APIView):
             info= "Success",
             status_code= status.HTTP_200_OK,
             data= data_list
+        )
+        return Response(context, status=status.HTTP_200_OK)
+    
+    
+class BookList(APIView):
+    """
+    A view to retrieve a list of books based on provided query parameters.
+
+    If both 'book_title' and 'author_name' are provided, it filters books by
+    title and author name. If only 'author_name' is provided, it filters books
+    by author name. If only 'book_title' is provided, it filters books by title.
+    If no query parameters are provided, it returns all books.
+
+    Returns:
+        Response: A response containing a list of serialized books.
+    """
+    def get(self, request):
+        
+        book_title = request.GET.get('book_title')
+        author_name = request.GET.get('author_name')
+        
+        if (book_title and author_name):
+            query_set = Book.objects.filter(title=book_title, bookauthormapper__author__name=author_name)
+        elif author_name:
+            query_set = Book.objects.filter(bookauthormapper__author__name=author_name)
+        elif book_title:
+            query_set = Book.objects.filter(title=book_title)
+        else:
+            query_set = Book.objects.all()
+            
+        serializer = BookSerializer(query_set, many=True)
+        
+        context = context_data_generator(
+            info= "Success",
+            status_code= status.HTTP_200_OK,
+            data= serializer.data
         )
         return Response(context, status=status.HTTP_200_OK)
